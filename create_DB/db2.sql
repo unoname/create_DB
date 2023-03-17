4973081d005ffab1e2635e4968ff46bef5aa84a6
aleks_pv <apanov08@gmail.com>
2023-03-18 00:13:44 +0400

    feat: add second variant

---
+++ create_DB/db2.sql
@@ -0,0 +1,80 @@
+
+CREATE DATABASE db_films;
+
+
+CREATE TABLE person (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL		
+);
+
+
+CREATE TABLE film (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(255) NOT NULL,
+  release_date DATE, 
+  country_production VARCHAR(255) NOT NULL,
+  slogan TEXT,
+  budget NUMERIC,
+  marketing NUMERIC,
+  us_gross NUMERIC,
+  worldwide_gross NUMERIC,
+  russia_premiere DATE,
+  worldwide_premiere DATE,
+  dvd_release DATE,
+  age_rating VARCHAR(10),
+  rating NUMERIC,
+  duration INTEGER,
+	description TEXT,
+  audio_track VARCHAR(255),
+  subtitles VARCHAR(255),
+  video_quality VARCHAR(10)
+	director INTEGER REFERENCES person_id
+	scenario INTEGER REFERENCES person_id
+	producer INTEGER REFERENCES person_id
+  operator INTEGER REFERENCES person_id
+	composer INTEGER REFERENCES person_id
+	artist INTEGER REFERENCES person_id
+	installation INTEGER REFERENCES person_id
+);
+
+CREATE TABLE actor (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE genre (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
+
+
+CREATE TABLE viewers (
+    id SERIAL PRIMARY KEY,
+    film_id INTEGER REFERENCES film(id),
+    country VARCHAR(255),
+    viewers_count INTEGER
+);
+
+
+
+CREATE TABLE film_genre (
+    film_id INTEGER REFERENCES film(id),
+    genre_id INTEGER REFERENCES genre(id),
+    PRIMARY KEY(film_id, genre_id)
+);
+
+
+CREATE TABLE film_viewers (
+    film_id INTEGER REFERENCES film(id),
+    viewers_id INTEGER REFERENCES viewers(id),
+    PRIMARY KEY(film_id, viewers_id)
+);
+
+
+CREATE TABLE film_actor (
+    film_id INTEGER REFERENCES film(id),
+    actor_id INTEGER REFERENCES actor(id),
+    role VARCHAR(255),
+    PRIMARY KEY(film_id, actor_id, role)
+);
+
